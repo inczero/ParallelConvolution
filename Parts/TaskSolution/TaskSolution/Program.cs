b37e688b56e4449b7c9e42ebdeead40a9b795beb
Incze Roland <incze_roland@yahoo.com>
2022-04-16 01:47:09 +0200

    add slicer

---
+++ Parts/TaskSolution/TaskSolution/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TaskSolution {
+    internal class Program {
+        static void Main(string[] args) {
+            Bitmap bitmap = new Bitmap("C:\\Users\\incze\\Desktop\\workdir\\train.jpg");
+
+            List<Bitmap> pieces = Slicer.RipApart(bitmap, 2);
+
+            //int fileIndex = 1;
+            //foreach (Bitmap piece in pieces) {
+            //    string filepath = "C:\\Users\\incze\\Desktop\\workdir\\sliced_" + fileIndex.ToString() + ".jpg";
+            //    piece.Save(filepath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //    fileIndex++;
+            //}
+
+            Kernel kernel = new Kernel();
+
+            kernel.GenerateGaussianFilter(23, 1.5);
+            kernel.PrintWeights();
+
+            Convolution convolution = new Convolution(pieces[1]);
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Bitmap filteredImage = convolution.Filter(kernel);
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed);
+
+            filteredImage.Save("C:\\Users\\incze\\Desktop\\workdir\\filtered.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            // draw boundary wide frame (125,125,125) to the picture -> cut to pieces with a boundary + piece length -> 
+            // create filtered image -> the size will be framed image width/height - boundary -> assemble filtered image parts
+            
+        }
+    }
+}
---
+++ Parts/TaskSolution/TaskSolution/Slicer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TaskSolution {
+    public static class Slicer {
+        public static List<Bitmap> RipApart(Bitmap image, int pieces) {
+            List<Bitmap> result = new List<Bitmap>();
+
+            int pieceSize = image.Width / pieces;
+
+            int pieceIndex = 0;
+
+            for (int i = 1; i <= pieces; i++) {
+                if (i == pieces) {
+                    pieceSize = image.Width - (i-1) * pieceSize;
+                }
+
+                Rectangle rect = new Rectangle(pieceIndex, 0, pieceSize, image.Height);
+
+                Bitmap slice = image.Clone(rect, image.PixelFormat);
+
+                result.Add(slice);
+
+                pieceIndex += pieceSize;
+            }
+
+            return result;
+        }
+    }
+}
