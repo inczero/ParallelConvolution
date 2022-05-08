c59c937f49c10cd877fc49657714a984f0c541cc
Incze Roland <incze_roland@yahoo.com>
2022-05-08 23:59:27 +0200

    delete old parts and bring up full solution

---
+++ Utilities/ConcurrentSliceProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelConvolution {
+    internal class ConcurrentSliceProcessor {
+        private Kernel kernel;
+
+        public ConcurrentSliceProcessor(Kernel kernel) { this.kernel = kernel; }
+
+        public void Work(ConcurrentBag<BitmapSlice> slices, ConcurrentBag<BitmapSlice> filtered) {
+            // will run until every slice from slices is processed
+            while (!slices.IsEmpty) {
+                BitmapSlice slice;
+
+                if (slices.TryTake(out slice)) {
+                    Bitmap filteredSlice = FilterUtils.Filter(slice.Image, kernel);
+
+                    slice.Image = filteredSlice;
+
+                    filtered.Add(slice);
+                }
+            }
+        }
+    }
+}
